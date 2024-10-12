301c82a60d873d5ea877f22403a32dfa8a95c7d6
Kyle <kyle12140@gmail.com>
2024-10-12 16:00:53 -0400

    added createTransaction and updated transaction to hold publickey and blockNumber

--- win/wincringe.py
+++ win/wincringe.py
@@ -1,11 +1,16 @@
 class Transaction:
-    def __init__(self, sender, reciever, amount, isSigned, isVerifiable):
+    def __init__(self, sender, reciever, amount, publickey, blockNumber):
      self.sender = sender
      self.reciever = reciever
      self.amount = amount
-     self.isSigned = isSigned
-     self.isVerifiable = isVerifiable #if signed by the private, can check w/ public
-    
-p1 = Transaction("Ali", "Bob", 100, True, True)
-    
+     self.publickey = publickey
+     self.blockNumber = blockNumber
+    def VerifyKey(publickey):
+        if isVerifiable: return True
+        else: return False
 
+A = []
+def createtrans(Transaction):
+    p1 = Transaction("Ali", "Bob", 100, 12, 0)
+    if p1.VerifyKey() == True: A.append(p1)
+    else: print(-1)
