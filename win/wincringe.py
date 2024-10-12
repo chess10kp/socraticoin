b5fea61e3cc5ed6258597816e8d2378460706bb1
Kyle <kyle12140@gmail.com>
2024-10-12 16:06:28 -0400

    added signed and gasFee into transaction, removed blockNumber, verifyKey and publickey

--- win/wincringe.py
+++ win/wincringe.py
@@ -1,16 +1,12 @@
 class Transaction:
-    def __init__(self, sender, reciever, amount, publickey, blockNumber):
+    def __init__(self, sender, reciever, amount, signed, gasFee):
      self.sender = sender
      self.reciever = reciever
      self.amount = amount
-     self.publickey = publickey
-     self.blockNumber = blockNumber
-    def VerifyKey(publickey):
-        if isVerifiable: return True
-        else: return False
+     self.signature = signature
+     self.gasFee = gasFee
 
 A = []
 def createtrans(Transaction):
     p1 = Transaction("Ali", "Bob", 100, 12, 0)
-    if p1.VerifyKey() == True: A.append(p1)
-    else: print(-1)
+    
