799456f9752312922294127f30aa62fde50e8bd1
richiii999 <richiii@umich.edu>
2024-10-12 16:14:51 -0400

    python import no work

--- richiii/Block.py
+++ richiii/Block.py
@@ -1,9 +1,21 @@
-# Block data structure
+### Block.py # richiii
+# Contains the 'Block' data structure
 
+from dataclasses import dataclass
+from win import wincringe
+
+@dataclass
 class Block:
 	"""Block in the Blockchain"""
-	var hash #
-	var nonce #
-	var data #
-	var blockReward # 
-	var prevHash # 
+	blockNumber:   int           # The number of this block 
+	currHash:      str           # The hash of this block
+	nonce:         int           # Randomized data used to compute the hash
+	data:          [Transaction] # Data stored in the block
+	blockReward:   int           # Reward given to the reward address 
+	rewardAddress: str           # Address which recieves block award
+	prevHash:      str           # Hash of the previous block (by number)
+
+
+x = Transaction("A", "B", 100, 0, 10)
+
+print(x.sender)
