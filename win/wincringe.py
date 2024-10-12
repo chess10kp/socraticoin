d9ff607b1db1c07f81039b2564b43be811f59ab9
Kyle <kyle12140@gmail.com>
2024-10-12 16:22:14 -0400

    added init w/ -111111

--- win/wincringe.py
+++ win/wincringe.py
@@ -1,12 +1,12 @@
 class Transaction:
-    def __init__(self, sender, reciever, amount, signed, gasFee):
+    def __init__(self, sender, reciever, amount, signed,  gasFee):
      self.sender = sender
      self.reciever = reciever
      self.amount = amount
-     self.signature = signature
+     self.signed = signed
      self.gasFee = gasFee
 
 A = []
 def createtrans(Transaction):
     p1 = Transaction("Ali", "Bob", 100, 12, 0)
-    
+    print(p1)
