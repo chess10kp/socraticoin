4bc7f4120574cd7caadcfe359e91f489fef5e169
Kyle <kyle12140@gmail.com>
2024-10-12 16:54:06 -0400

    added print function for transaction

--- win/wincringe.py
+++ win/wincringe.py
@@ -5,8 +5,15 @@ class Transaction:
      self.amount = amount
      self.signed = signed
      self.gasFee = gasFee
-
+    def __str__(self):
+        return str(self.sender + " " + 
+                   self.reciever + " " + 
+                   str(self.amount) + " " + 
+                   self.signed + " " + 
+                   str(self.gasFee))
 A = []
 def createtrans(Transaction):
     p1 = Transaction("Ali", "Bob", 100, 12, 0)
     print(p1)
+x = Transaction("Ali", "Bob", 100, 12, 0)
+print(x.sender)
