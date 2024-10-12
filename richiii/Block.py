80be8e35d5a6af85d97cac327214e304ed5742e8
richiii999 <richiii@umich.edu>
2024-10-12 16:52:39 -0400

    Block.py and Test script

--- richiii/Block.py
+++ richiii/Block.py
@@ -2,20 +2,33 @@
 # Contains the 'Block' data structure
 
 from dataclasses import dataclass
-from win import wincringe
+from win.wincringe import *
 
 @dataclass
 class Block:
 	"""Block in the Blockchain"""
-	blockNumber:   int           # The number of this block 
-	currHash:      str           # The hash of this block
-	nonce:         int           # Randomized data used to compute the hash
-	data:          [Transaction] # Data stored in the block
-	blockReward:   int           # Reward given to the reward address 
-	rewardAddress: str           # Address which recieves block award
-	prevHash:      str           # Hash of the previous block (by number)
+	def __init__(self,a,b,c,d,e,f,g):
+		self.blockNumber = a
+		self.currHash = b
+		self.nonce = c
+		self.data = d
+		self.blockReward = e
+		self.rewardAddress = f
+		self.prevHash = g
 
+	blockNumber:   int               # The number of this block 
+	currHash:      str               # The hash of this block
+	nonce:         int               # Randomized data used to compute the hash
+	data:          list[Transaction] # Data stored in the block
+	blockReward:   int               # Reward given to the reward address 
+	rewardAddress: str               # Address which recieves block award
+	prevHash:      str               # Hash of the previous block (by number)
 
-x = Transaction("A", "B", 100, 0, 10)
-
-print(x.sender)
+	def __str__(self):
+		return ( str(self.blockNumber)  + " "
+			   +     self.currHash      + " "
+			   + str(self.blockReward)  + " "
+			   +     self.rewardAddress + " "
+			   +     self.prevHash      + " "
+			   + str(self.nonce)        + "\n"
+			   + str(self.data)         + "\n" )
---
+++ richiii/Test.py
@@ -0,0 +1,15 @@
+### Test.py # richiii
+# Creates 3 transactions, adds them to a block, and prints out the block
+
+from richiii.Block import * # Block Class
+from win.wincringe import * # Transaction Class
+
+t1 = Transaction("A", "B", 100, 0, 9)
+t2 = Transaction("B", "C", 50,  0, 5)
+t3 = Transaction("C", "A", 10,  0, 3)
+
+tList = [t1, t2, t3]
+
+block = Block(0, "asdf", 999, tList, 10, "A", "bruh")
+
+print(block)
