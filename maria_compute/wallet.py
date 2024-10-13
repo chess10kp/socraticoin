3a0f55f469b69885c8c148f82765d95726443d84
richiii999 <richiii@umich.edu>
2024-10-13 05:51:21 -0400

    commit these nuts

--- maria_compute/wallet.py
+++ maria_compute/wallet.py
@@ -17,10 +17,7 @@ class user_wallete:
         return priv_to_str(self.private_key)[-40:-36]
 
     def get_public_str_full(self):
-        fullstr = (pub_to_str(self.public_key)).split('\n')
-        fullstr = fullstr[1:-1]
-        print(fullstr)
-        return fullstr
+        return pub_to_str(self.public_key)[:]
     def get_private_str_full(self):
         return priv_to_str(self.private_key)[-52:-36]
     
