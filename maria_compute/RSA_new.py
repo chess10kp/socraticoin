72877527f2214063ce253b752bc767d8d86488e6
maria <mew2two1@outlook.com>
2024-10-12 21:00:15 -0400

    mess

--- maria_compute/RSA_new.py
+++ maria_compute/RSA_new.py
@@ -5,10 +5,6 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import utils
 import hashlib
 
-#private_key = rsa.generate_private_key(
- #   public_exponent=65537,
-  #  key_size=2048
-#)
 def gneratebothkey():
     private_key = ec.generate_private_key(
         ec.SECP384R1()
@@ -25,7 +21,7 @@ def RSA_sig(pk,dat):
         ec.ECDSA(hashes.SHA256())
 
     )
-    print("signiture is this: ",signature)
+    #####print("signiture is this: ",signature)
     return signature
 
 
