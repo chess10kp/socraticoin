af6247e0792e182e810fc89e2428e5c9ca611de9
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2024-10-21 23:55:43 -0400

    chore(formatting): clean up linter errors and formatting

--- maria_compute/wallet.py
+++ maria_compute/wallet.py
@@ -1,5 +1,6 @@
-#create a create wallet that returns the funciton it'sef
-from maria_compute.RSA_new import * 
+# create a create wallet that returns the funciton it'sef
+from maria_compute.RSA_new import pub_to_str, priv_to_str, gneratebothkey
+
 
 class user_wallete:
     def __init__(self):
@@ -8,32 +9,35 @@ class user_wallete:
 
     def get_public_key(self):
         return self.public_key
+
     def get_private_key(self):
         return self.private_key
-    
+
     def get_public_str(self):
         return pub_to_str(self.public_key)[-40:-36]
+
     def get_private_str(self):
         return priv_to_str(self.private_key)[-40:-36]
 
     def get_public_str_full(self):
         return pub_to_str(self.public_key)
+
     def get_private_str_full(self):
         return priv_to_str(self.private_key)
-    
+
     def get_crypto(self):
-        temp= int(self.ammount_of_crypto)
+        temp = int(self.ammount_of_crypto)
         return temp
-    
-    def check_balance(self,amt):
-        
+
+    def check_balance(self, amt):
         if (self.get_crypto() - amt) < 0:
-           ##  print("broke boy")
-             return False
+            ##  print("broke boy")
+            return False
         else:
             return True
 
-'''
+
+"""
 A = user_wallete(0)
 
 addr = A.get_public_key()
@@ -47,4 +51,4 @@ amt = int(input("insert ammount you wanna spend "))
 if(S.check_balance(amt)):
     print("amount transacted and sent to :",A.get_public_key())
 
-'''
+"""
